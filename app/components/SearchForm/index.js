4bc02948bd0055b2eaa4696f6f96226dfecb9d3e
aufimtsev <fimak@bk.ru>
2017-10-02 00:11:40 +0600

    search form

---
+++ app/components/SearchForm/index.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Select from 'react-select'
+import { Grid, Row, Col, Form, FormGroup, Button, ButtonGroup } from 'react-bootstrap'
+
+import 'react-select/dist/react-select.css'
+import style from './style.css'
+
+export default class SearchForm extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <Grid>
+        <Row>
+          <Col md={3} mdOffset={2}>
+            <h2 className={style.title}>TripSorter</h2>
+            <Form horizontal onSubmit={this.props.onSubmitForm} className={style.root}>
+              <FormGroup>
+                <Select name="from"/>
+              </FormGroup>
+              <FormGroup>
+                <Select name="to"/>
+              </FormGroup>
+              <FormGroup>
+                <ButtonGroup justified>
+                  <Button href="#">Cheapest</Button>
+                  <Button href="#">Fastest</Button>
+                </ButtonGroup>
+              </FormGroup>
+              <FormGroup justified>
+                <Button bsStyle="success" block>Search</Button>
+              </FormGroup>
+            </Form>
+          </Col>
+        </Row>
+      </Grid>
+    )
+  }
+}
--- app/containers/TestPage/index.js
+++ app/containers/TestPage/index.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+import SearchForm from '../../components/SearchForm'
+import style from './style.css'
 
 export default class TestPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
 
   render() {
     return (
-      <article>
+      <article className={style.TestPage}>
         <Helmet>
           <title>TripSorter</title>
-          <meta name="description" content="Trip sorter" />
+          <meta name="description" content="Trip sorter"/>
         </Helmet>
-        <div>
-          <h2>TripSorter</h2>
-            <form onSubmit={this.props.onSubmitForm}>
-
-            </form>
-        </div>
+        <SearchForm/>
       </article>
-    );
+    )
   }
 }
